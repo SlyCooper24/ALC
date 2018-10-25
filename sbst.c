b70854e356bd1853876d9607d08f93f64bb04e0f
SlyCooper24 <42280910+SlyCooper24@users.noreply.github.com>
2018-10-24 22:48:46 -0300

    Update sbst.c

--- sbst.c
+++ sbst.c
@@ -3,46 +3,35 @@
 int main ()
 {
 
-    int linhas,colunas;
-    linhas = 3;
-    colunas = 3;
+    //int numerodelinhas,numerodecolunas;
+    //numerodelinhas = 3;
+    //numerodecolunas = 3;
 
-    float soma,a[linhas][colunas],b[colunas],x[linhas];
+    //float soma,a[numerodelinhas][numerodecolunas],b[numerodecolunas],x[numerodelinhas];
+    
+    //SubsParaTras(3, 3, )
+    
+    
+    
+    return 0;
+}
 
-    for ( int i = 0; i<linhas; i++ )
+
+void SubsParaTras(int numerodelinhas, int numerodecolunas, float MatrizA[numerodelinhas][numerodecolunas])
+{
+    float VetorResultados[numerodecolunas], VetorIncognitas[numerodelinhas];
+    VetorIncognitas[numerodelinhas - 1] = (VetorResultados[numerodecolunas - 1] / MatrizA[numerodelinhas - 1][numerodecolunas - 1]);
+    int soma = 0;
+    for(int linhaatual = numerodelinhas - 2; linhaatual >= 0; linhaatual--)
     {
-        for ( int j = 0; j<colunas; j++ )
+        for(int colunaatual = linhaatual + 1; colunaatual != numerodecolunas; colunaatual++)
         {
-            printf ("Elemento de A[%d][%d] = ", i, j);
-            scanf ("%f", &a[ i ][ j ]);
+            soma += MatrizA[linhaatual][colunaatual] * VetorIncognitas[colunaatual];
         }
+        VetorIncognitas[linhaatual] = ((VetorResultados[linhaatual] - soma) / MatrizA[linhaatual][linhaatual]);
     }
-
-    if(a[linhas - 1][0] != 0)
+    for(int linhaatual = 0; linhaatual < numerodelinhas; linhaatual++)
     {
-        printf("A matriz nao e triangular inferior");
-        return 0;
-    }
-
-    for(int i = 0; i<colunas; i++)
-    {
-        printf ("Elemento de B[%d] = ", i);
-        scanf("%f", &b[ i ]);
-        x[i] = 0;
-    }
-    x[linhas - 1] = (b[colunas - 1]/a[linhas - 1][colunas - 1]);
-    for(int i = linhas-2; i>=0; i--)
-    {
-        soma = 0;
-        for(int j = i+1; j != colunas; j++)
-        {
-            soma += a[ i ][ j ] * x [ j ];
-        }
-        x[ i ] = ((b[ i ] - soma)/a[ i ][ i ]);
-    }
-
-    for(int i = 0; i<linhas; i++)
-    {
-        printf("o valor de x%d e: %.2f\n",i+1,x[i]);
+        printf("o valor de x%d e: %.2f\n",linhaatual + 1, VetorIncognitas[linhaatual]);
     }
 }
